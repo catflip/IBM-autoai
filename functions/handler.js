fdd9097bd825497f8a9e80b2213bcc8ea2d8dcfb
febriano <rinoakbr@gmail.com>
2021-05-01 22:15:28 +0700

    bismillah menang aamiin

--- functions/handler.js
+++ functions/handler.js
@@ -1,7 +1,8 @@
+'use strict';
 const axios = require("axios")
 
-// NOTE: you must manually enter your API_KEY below using information retrieved from your IBM Cloud
-const API_KEY = "n196x_zB-wCZ6ya9gakTWDgbuSbGQL4Hpp8k34Limixc";
+async function autoai(param) {
+  const API_KEY = param.api_key;
 const params = new URLSearchParams()
 params.append('grant_type', 'urn:ibm:params:oauth:grant-type:apikey')
 params.append('apikey', API_KEY)
@@ -12,10 +13,10 @@ const config = {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
 }
-axios.post("https://iam.cloud.ibm.com/identity/token", params, config)
-  .then((result) => {
+const result=await axios.post("https://iam.cloud.ibm.com/identity/token", params, config)
+  
   const token=result.data.access_token
-  const config = {
+  const configs = {
     headers: {
       "Accept": "application/json",
       "Authorization": "Bearer " + token,
@@ -23,7 +24,11 @@ axios.post("https://iam.cloud.ibm.com/identity/token", params, config)
     }
   }
   const fields=["Age","Gender","Polyuria","Polydipsia","sudden weight loss","weakness","Polyphagia","Genital thrush","visual blurring","Itching","Irritability","delayed healing","partial paresis","muscle stiffness","Alopecia","Obesity"]
-  const value=[20,"Male",null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+  const value=[param.age,param.gender,param.polyuria,param.polydipsia,param.sudden_weight_loss,param.weakness,param.polyphagia,param.genital_thrush,param.visual_blurring,param.itching,param.irritability,param.delayed_healing,param.partial_paresis,param.muscle_stiffness,param.alopecia,param.obesity]
   const payload={"input_data": [{fields, "values": [value]}]}
-  axios.post("https://jp-tok.ml.cloud.ibm.com/ml/v4/deployments/9567b9b7-f5a6-4428-a73b-3a7065c37f4b/predictions?version=2021-04-30",payload,config).then(a=>console.log(a.data))
-  })
+  const res=await axios.post("https://jp-tok.ml.cloud.ibm.com/ml/v4/deployments/9567b9b7-f5a6-4428-a73b-3a7065c37f4b/predictions?version=2021-04-30",payload,configs)
+  return {data:res.data}
+  
+}
+
+exports.autoai = autoai;
